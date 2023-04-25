b3477b355a02019ac10c8bcefdcbdf018a42320d
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-04-26 08:35:27 +1200

    feat: init commit

---
+++ project-01-learner-gradebook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_01_learner_gradebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
